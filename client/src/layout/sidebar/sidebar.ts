de9889e5021ec35bdb8466d9c462125b1a64624d
April Gale Guevara <aprilgale.swe@gmail.com>
2026-04-11 11:24:09 +0800

    Add other pages/items on the sidebar

--- client/src/layout/sidebar/sidebar.ts
+++ client/src/layout/sidebar/sidebar.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Icon, iconPaths } from '../../core/enums/icon-enum';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,42 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   imports: [RouterLink, RouterLinkActive],
 })
 export class Sidebar {
+  protected readonly iconPaths = iconPaths;
+  protected readonly navItems: NavItem[] = [
+    { label: 'Dashboard', route: '/dashboard', icon: Icon.Squares2x2 },
+    {
+      label: 'My Requests',
+      route: '/',
+      icon: Icon.Inbox,
+      children: [
+        { label: 'Drafts', route: '/', icon: Icon.PencilSquare },
+        { label: 'Submitted', route: '/', icon: Icon.PaperAirplane },
+      ],
+    },
+    {
+      label: 'My Approvals',
+      route: '/',
+      icon: Icon.ClipboardDocumentCheck,
+      children: [
+        { label: 'Upcoming', route: '/', icon: Icon.CalendarDays },
+        { label: 'Pending', route: '/', icon: Icon.Clock },
+        { label: 'Completed', route: '/', icon: Icon.CheckCircle },
+      ],
+    },
+    { label: 'All Requests', route: '/', icon: Icon.QueueList },
+  ];
+
   protected collapsed = signal(false);
 
   protected toggle() {
     this.collapsed.update((v) => !v);
   }
+}
+
+// TODO: Transfer in a dedicated file
+interface NavItem {
+  label: string;
+  route: string;
+  icon: Icon;
+  children?: NavItem[];
 }
